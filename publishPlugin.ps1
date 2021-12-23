a5887611e91d65b50113a6bf30d3bb396d288a0c
Christophe Commeyne <ccommeyne@GENETEC.COM>
2021-12-22 19:04:33 -0500

    Fix the package ID to be recognized by Visual Studio.

--- publishPlugin.ps1
+++ publishPlugin.ps1
@@ -1,9 +1,9 @@
 Param(
     [string]$Configuration = "Release",
     [Parameter(Mandatory=$true)]
-    [string]$Version,
-    [Parameter(Mandatory=$true)]
-    [string]$ApiKey
+    [string]$Version
+    #[Parameter(Mandatory=$true)]
+    #[string]$ApiKey
 )
 
 Set-StrictMode -Version Latest
@@ -17,4 +17,4 @@ $ChangelogText = ([Regex]::Matches([System.IO.File]::ReadAllText("CHANGELOG.md")
 
 Invoke-Exe $MSBuildPath "/t:Restore;Rebuild;Pack" "$SolutionPath" "/v:minimal" "/p:Configuration=$Configuration" "/p:PackageOutputPath=$OutputDirectory" "/p:PackageVersion=$Version" "/p:PackageReleaseNotes=`"$ChangelogText`""
 $PackageFile = "$OutputDirectory\$PluginId.$Version*.nupkg"
-Invoke-Exe $NuGetPath push $PackageFile -Source "https://plugins.jetbrains.com/api/v2/package" -ApiKey $ApiKey
+#Invoke-Exe $NuGetPath push $PackageFile -Source "https://plugins.jetbrains.com/api/v2/package" -ApiKey $ApiKey
