56c864fccc1dd0ce8d9cb9b67b3eea3cd39021b4
kzryzstof <BR7ygoYD$WVf>
2022-01-07 19:54:28 -0500

    Adds more information to the plugin itself.

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/sampleplugin/SampleAction.kt
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.rider.plugins.mockingspongebob
-
-import com.jetbrains.rider.actions.base.RiderAnAction
-
-class SampleAction : RiderAnAction(
-        "SampleAction",
-        "Do Something",
-        "Do Something Description")
